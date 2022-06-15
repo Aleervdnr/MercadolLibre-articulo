bbc24985e00b6fd00eac8b8ad2bd098912d8ed1c
Alejo <alejonahuelrott@gmail.com>
2022-06-15 20:52:00 -0300

    Add componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Componentes/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
     </div>
   );
 }
