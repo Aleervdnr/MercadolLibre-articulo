a466e0f073bebb1ef2a6c403045ee9cfc616b3df
Alejo <alejonahuelrott@gmail.com>
2022-07-05 22:45:57 -0300

    add main, share content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Componentes/Header/Header';
+import Main from './Componentes/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Componentes/Main/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./Main.css"
+import {BsChevronRight} from "react-icons/bs"
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="row">
+        <div className="relacionado">
+          <div className="interes">
+            <span>También puede interesarte:</span>
+            <a href="#">link</a>
+            -
+            <a href="#">link</a>
+            -
+            <a href="#">link</a>
+            -
+            <a href="#">link</a>
+            -
+            <a href="#">link</a>
+            -
+            <a href="#">link</a>
+          </div>
+
+          <div className="breadcrumb">
+            <a href="#" className='breadcrumb_volver'>Volver al listado</a>
+            <nav>
+              <a href="#" className="breadcrumb_link">Herramientas</a>
+              <BsChevronRight className='asd'/>
+              <a href="#" className="breadcrumb_link">Herramientas Eléctricas</a>
+              <BsChevronRight className='asd'/>
+              <a href="#" className="breadcrumb_link">Perforación</a>
+              <BsChevronRight className='asd'/>
+              <a href="#" className="breadcrumb_link">Taladros</a>
+              <BsChevronRight className='asd'/>
+              <a href="#" className="breadcrumb_link">De Mano</a>
+            </nav>
+          </div>
+
+        </div>
+
+        <div className="share">
+          <a href="#" className='share_link'>Compartir</a>
+          <a href="#">Vender uno igual</a>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Main
