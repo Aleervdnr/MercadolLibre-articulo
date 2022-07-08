f7c7056628b7999fb7a62c2e1a1f124158c014f0
Alejo <alejonahuelrott@gmail.com>
2022-07-07 22:27:52 -0300

    box-title desktop

--- src/Componentes/Producto/Producto.jsx
+++ src/Componentes/Producto/Producto.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Producto.css"
 import Galeria from '../Galeria/Galeria'
+import {AiOutlineHeart, AiFillHeart} from "react-icons/ai"
+import {MdVerified} from "react-icons/md"
 
 export default function Producto() {
+
+  const [fav, setFav] = useState(false)
   return (
     <div className="producto">
-      <div className="column-left">
-          <Galeria/>
-        <div className="texto"></div>
-      </div>
+        <Galeria/>
 
-      <div className="column-right"></div>
+        <div className="texto">
+          <div className="principal_card">
+            <span className="subtitle">2022 | 0 km · Publicado hace 10 meses</span>
+            <div className="container-title">
+              <h1 className="title">Honda New Wave 110 S 2022 0KM</h1>
+              <div className="fav" onClick={()=> setFav(!fav)}>
+                {fav ? <AiFillHeart className='heart-icon-active'/> : <AiOutlineHeart className='heart-icon'/>}
+              </div>
+            </div>
+
+            <div className="verify">
+              Vendedor con <span>identidad verificada</span> <MdVerified className='verify-icon'/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
